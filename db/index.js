342412ebfaf50dab5e2535ec0b2a2177d595bcc6
David Unuigbe <mowems@gmail.com>
2023-11-04 02:04:15 +0200

    Preparing for deployment

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect('mongodb+srv://moadmin:chewychewy@cluster0.bnaep.mongodb.net/tvshows?retryWrites=true&w=majority');
